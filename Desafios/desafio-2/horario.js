d9e93bb2db824dcc2c2d9c2a79a7f480f103c587
MatheusAndrade03 <139627645+MatheusAndrade03@users.noreply.github.com>
2024-06-01 18:46:51 -0300

    desadio 2

---
+++ Desafios/desafio-2/horario.js
@@ -0,0 +1,18 @@
+
+var  data = new Date()
+var  hora = data.getHours
+
+var horas = window.document.getElementById("horas")
+  
+if(hora > 12){
+
+
+
+
+
+}else if(){
+
+
+
+    
+}
---
+++ Exercicios/ex007/semana.js
@@ -0,0 +1,65 @@
+var  data = new Date()
+
+var  dia = 5
+/*
+
+segunda 
+
+terça
+
+quarta 
+
+quinta 
+
+sexta 
+
+sabado
+
+domingo
+
+*/
+
+
+switch(dia){
+
+
+case 0 :
+    console.log("Domingo")
+
+break;
+
+
+case 1:
+console.log("Segunda")
+break;
+case 2:
+
+console.log("terça")
+
+break
+
+case 3:
+   console.log("quarta")
+break;
+
+
+case 4 :
+
+console.log("Quinta")
+
+break;
+
+
+
+
+case 5 :
+console.log("Sexta")
+
+break;
+
+case 6 :
+console.log("sabado")
+break;
+
+
+}
