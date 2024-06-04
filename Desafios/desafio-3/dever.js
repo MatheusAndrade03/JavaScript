2fb23c6f530cde9aa3671bd44099920e0cd3b004
MatheusAndrade03 <139627645+MatheusAndrade03@users.noreply.github.com>
2024-06-04 13:08:50 -0300

    att

--- Desafios/desafio-3/dever.js
+++ Desafios/desafio-3/dever.js
@@ -1,13 +1,148 @@
 
 
 function clicou(){
+
+        // imagens
+        var img = document.querySelector("#img")
+        // div imagem 
+        var divImagem = document.getElementById("imagem")
+        //body
         var corpo = window.document.body
-        var nascimento=window.document.getElementById("inumero")
+        // numero digitado 
+        var nDigitado=window.document.getElementById("inumero")
+        var nascimento= nDigitado.value
+        // ano atual
         var data = new Date()
-        var idade =  nascimento
-        
+        var ano =  data.getFullYear()
+        //Idade 
+        var idade = ano- nascimento
+        // verificação de compo vazio ou maior que a idade
+        if(nascimento.length == 0 || nascimento > ano){
+                        window.alert("ERRO!")
+
+        }else
+        {       
+                // radio com o genero
+                var fSex= document.getElementsByName("sexo")
+                
+                // genero
+                var genero =''
+
+                if(fSex[0].checked){
+                        genero ="Homem"
+
+                        divImagem.style.display="flex"
+                        divImagem.innerHTML=`Detectamos um ${genero} de ${idade}anos`
 
 
+
+                if(idade <= 5 ){
+                                //bebe
+                               
+
+
+
+                } else if(idade >5 && idade <=10){
+                                //criança
+
+
+
+
+
+
+                }else if(idade >10 && idade<18){
+                        // Adolecente
+
+
+
+
+
+
+                }else if(idade >=18 && idade <30){
+                        //jovem
+
+
+
+
+
+                }else if (idade >=30 && idade < 60){
+                        //Adulto
+
+
+
+
+
+
+                }else if(idade >=60){
+                        //velho
+
+
+
+                }
+                        
+
+                }else if (fSex[1].checked){
+
+                        genero="Mulher"
+                        divImagem.style.display="flex"
+                        divImagem.innerHTML=`Detectamos um ${genero} de ${idade}anos`
+
+                if(idade <= 5 ){
+                                //bebe
+                               
+
+
+
+                } else if(idade >5 && idade <=10){
+                                //criança
+
+
+
+
+
+
+                }else if(idade >10 && idade<18){
+                        // Adolecente
+
+
+
+
+
+
+                }else if(idade >=18 && idade <30){
+                        //jovem
+
+
+
+
+
+                }else if (idade >=30 && idade < 60){
+                        //Adulto
+
+
+
+
+
+
+                }else if(idade >=60){
+                        //velho
+
+
+
+                }
+                        
+                }
+
+                        
+
+                
+                        
+
+
+
+
+        }
+      
 
 
 
