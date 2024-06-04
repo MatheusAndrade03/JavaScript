d180256bcfb4834fd0456e86c8995f96b71aa431
MatheusAndrade03 <139627645+MatheusAndrade03@users.noreply.github.com>
2024-06-04 09:10:07 -0300

    Atualização

--- Desafios/desafio-2/horario.js
+++ Desafios/desafio-2/horario.js
@@ -1,18 +1,38 @@
 
+
+function carregar(){
+
 var  data = new Date()
-var  hora = data.getHours
+var  hora = 15
+var img = window.document.getElementById('imgHoras')
+var horas = window.document.getElementById('horas')
+var corpo= window.document.body
 
-var horas = window.document.getElementById("horas")
+if( hora >=5 && hora < 12){
+
+  horas.innerHTML=`Agora são ${hora} Horas`
+  img.src="manha.jpeg"
+  corpo.style.backgroundColor="#ffdaac"
+
+
+}
+else if( hora < 5 || hora > 19 ){
+   horas.innerHTML=`Agora são ${hora} Horas`
+   corpo.style.backgroundColor="#372b48"
   
-if(hora > 12){
+   img.src="noite.jpeg"
+}else if(hora <= 19){
+
+   horas.innerHTML=`Agora são ${hora} Horas`
+  img.src="tarde.jpeg"
+  corpo.style.backgroundColor="#ad7646"
+  
+
+}
 
 
 
 
 
-}else if(){
 
-
-
-    
 }
---
+++ Desafios/desafio-3/dever.js
@@ -0,0 +1,17 @@
+
+
+function clicou(){
+        var corpo = window.document.body
+        var nascimento=window.document.getElementById("inumero")
+        var data = new Date()
+        var idade =  nascimento
+        
+
+
+
+
+
+
+
+
+}
