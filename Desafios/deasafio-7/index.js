12ad30479777c355f9264412f03f48d61a691037
MatheusAndrade03 <139627645+MatheusAndrade03@users.noreply.github.com>
2024-09-30 11:48:34 -0300

    att

--- Desafios/deasafio-7/index.js
+++ Desafios/deasafio-7/index.js
@@ -4,8 +4,20 @@ const btnGo = document.querySelector("#btn-vai")
 const valor = document.querySelector("#inumero")
 const img = document.querySelector("#img")
 const conteudo = document.querySelector("#content")
+const keys=['name','status', 'species','gender','origin','image','episode']
 
 // functions 
+
+const buildResult=(result)=>{
+    const newObject={};
+    keys.map((key)=>document.getElementById(key))
+    .map((elem)=>{
+            elem.checked && (newObject[elem.name]= result[elem.name]);
+
+    });
+
+        return newObject;
+}
 
  function  fetchApi(value){
 
@@ -16,15 +28,22 @@ const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
     console.log(data)
     return  data;
 })
+
 return result
 }
+
+
+
+
 // botão
 btnGo.addEventListener("click", async (event)=>{
     event.preventDefault();
 
     const resultado = await fetchApi(valor.value)
 
-    conteudo.textContent= `${JSON.stringify (resultado,undefined,2)}`;
+    // conteudo.textContent= `${JSON.stringify (resultado,undefined,2)}`;
+   
+    conteudo.textContent = `${JSON.stringify ( buildResult(resultado),undefined,2)}`
     img.src=`${resultado.image}`
 
 })
