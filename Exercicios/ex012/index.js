61e845831397289326b03de150d3ec4832384611
Matheus Henrique de Andrade Pellegrini <139627645+MatheusAndrade03@users.noreply.github.com>
2024-11-07 08:00:34 -0300

    att

--- Exercicios/ex012/index.js
+++ Exercicios/ex012/index.js
@@ -1,31 +1,4 @@
 
 
-const usuarios= [
-    {
-        nome: "Matheus",
-        idade: 23,
-        email: "matheus@gmail.com"
-    } 
-    ,
-    {
-        nome:"Joao",
-        idade:20,
-        email: "joao@gmail.com"
-    },
-    {
-        nome: "Maria",
-        idade: 25,
-        email: "maria@gmail.com"    
-    }
 
 
-
-];
-
-
-localStorage.setItem(usuarios[0].nome, usuarios[0].json());
-localStorage.setItem("senha","123456");
-
-let usuario = localStorage.getItem('usuario');
-
-console.log(usuario);
